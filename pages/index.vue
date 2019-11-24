cfa7db3d003eb8a400650404d932e061c2245632
samuraikun <tazitaziawawa@gmail.com>
2019-11-25 00:50:22 +0900

    画面の微修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,12 @@
 <template lang="pug">
-HeaderAndMain(:wideHeader="true")
-  template(slot="header")
-    AppHeader
-  template(slot="main")
-    ProductList
+  ProductList
 </template>
 
 <script>
-import HeaderAndMain from '../components/templates/HeaderAndMain'
-import AppHeader from '../components/organisms/AppHeader'
 import ProductList from '../components/organisms/ProductList'
 
 export default {
   name: 'App',
-  components: { HeaderAndMain, AppHeader, ProductList }
+  components: { ProductList }
 }
 </script>
